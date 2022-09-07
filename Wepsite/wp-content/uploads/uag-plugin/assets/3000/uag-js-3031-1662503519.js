e2b1850148c027a13fe1e8806948e7d06d0eda7e
bayanabu-alroos <bayan.adnan.alroos@gmail.com>
2022-09-07 20:21:27 +0300

    add website ecommerce

---
+++ Wepsite/wp-content/uploads/uag-plugin/assets/3000/uag-js-3031-1662503519.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	UAGBForms.init( {"block_id":"e2ce77bb","reCaptchaEnable":false,"reCaptchaType":"v2","reCaptchaSiteKeyV2":"","reCaptchaSecretKeyV2":"","reCaptchaSiteKeyV3":"","reCaptchaSecretKeyV3":"","afterSubmitToEmail":"","afterSubmitCcEmail":"","afterSubmitBccEmail":"","afterSubmitEmailSubject":"Form Submission","sendAfterSubmitEmail":true,"confirmationType":"message","hidereCaptchaBatch":false,"captchaMessage":"Please fill up the above captcha.","confirmationUrl":""}, '.uagb-block-e2ce77bb' );
+});
+ })
---
+++ Wepsite/wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1663168292;s:7:"authKey";s:64:"D127bFt}tDxj0^<SBH-)Kjcq5}; %z#~lz,OpX[eg,8>%YDa/mlAJtj7l7Q(6b ;";s:7:"version";s:5:"1.0.5";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:1444;}
